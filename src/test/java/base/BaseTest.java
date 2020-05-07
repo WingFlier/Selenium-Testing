3ca8f54e8fbfefdcf8a838e5a0d2dddc7aa635d2
Wingflier <tiko1581@mail.ru>
2020-05-07 14:19:41 +0400

    Screenshot taking part added

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,16 @@
 package base;
 
+import com.google.common.io.Files;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,30 +22,41 @@ import pages.HomePage;
 
 public class BaseTest
 {
-	protected WebDriver driver;
+    protected WebDriver driver;
 
-	protected HomePage homePage;
-	protected URL url;
-	protected String website;
+    protected HomePage homePage;
+    protected URL url;
+    protected String website;
 
-	protected LoginTest loginTest;
+    protected LoginTest loginTest;
 
-	@BeforeClass
-	public void setUp() throws MalformedURLException
-	{
+    @BeforeClass
+    public void setUp() throws MalformedURLException
+    {
 		/*System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
 		driver.register(new EventReporter());*/
 
-		url = new URL("http://localhost:4444/wd/hub");
-		website = "http://www.quizful.net/test";
+        url = new URL("http://localhost:4444/wd/hub");
+        website = "http://www.quizful.net/test";
 
-	}
+    }
 
 
-	@AfterMethod
-	public void off()
-	{
-		driver.quit();
-	}
+    @AfterMethod
+    public void off(ITestResult result)
+    {
+        if (result.getStatus() == ITestResult.FAILURE)
+        {
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			try
+			{
+				Files.move(screenshot, new File("src/main/resources/screenshots" + result.getName() + ".png"));
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+        driver.quit();
+    }
 }
